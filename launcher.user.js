afcdb7763cf4a2fffcef6259cff6db9c59c202ba
nanoking <nanoking5050@gmail.com>
2015-07-26 19:02:52 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1294,7 +1294,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["TURKEY"],
+                names = ["YiyeninANasınınAq"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
